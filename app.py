be0653d51ca23f8577f499d1c68b2d22cab9bdeb
Josh Burke <joshburke.school@gmail.com>
2018-09-17 16:30:53 -0500

    part1

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def create_response(
     data: dict = None, status: int = 200, message: str = ""
 ) -> Tuple[Response, int]:
     """Wraps response in a consistent format throughout the API.
-    
+
     Format inspired by https://medium.com/@shazow/how-i-design-json-api-responses-71900f00f2db
     Modifications included:
     - make success a boolean since there's only 2 values
@@ -53,6 +53,12 @@ def mirror(name):
 
 
 # TODO: Implement the rest of the API here!
+
+@app.route("/users")
+def users():
+    data = db.get("users")
+    dic = {"users": data}
+    return create_response(dic)
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
